7a49dfa257c6684b84fca55397a7dac49d0670ad
roni <roni78rm@gmail.com>
2023-02-15 12:37:55 +0530

    resolved the issue

--- library/app.js
+++ library/app.js
@@ -19,43 +19,43 @@ function book(book_name,book_author,book_pages,book_read) {
 
 function addBookTolibrary(book) {
     books.push(book);
-    displayTable(books);
+    displayTable(book);
    
 }
 
-function displayTable(books) {
-    for (i = 0; i < books.length; i++){
+function displayTable(book) {
+   
         let newrow = document.createElement('tr');
         for (j = 0; j <= 4; j++){
             let newcell = document.createElement("td");
-            if (j == 0) newcell.textContent = books[i].book_name;
-            else if (j == 1) newcell.textContent = books[i].book_author;
-            else if (j == 2) newcell.textContent = books[i].book_pages;
-            else if (j == 3) newcell.textContent = books[i].book_read;
+            if (j == 0) newcell.textContent = book.book_name;
+            else if (j == 1) newcell.textContent = book.book_author;
+            else if (j == 2) newcell.textContent = book.book_pages;
+            else if (j == 3) newcell.textContent = book.book_read;
             else if (j==4) {
                 let button_del = document.createElement('button');
-                // button_del.setAttribute("class", "btn");
                 button_del.classList.add('btn');
                 button_del.textContent = 'del';
+                button_del.addEventListener('click', function () {
+                    let tr_element = button_del.parentNode.parentNode;
+                    table_body.removeChild(tr_element);
+                });
                 newcell.appendChild(button_del);
             } 
             newrow.appendChild(newcell);
         }
         table_body.appendChild(newrow);
-    }
 }
-let button_delete = document.querySelector('.btn');
-button_delete.addEventListener('click', function () {
-    let tr_element = button_delete.parentNode.parentNode;
-    table_body.removeChild(tr_element);
-});
+
+
+
 
 
 function getBookDetails() {
    book_name = document.querySelector('input[name="name"]').value;
    book_author = document.querySelector('input[name="author"]').value;
    book_pages = document.querySelector('input[name="pages"]').value;
-   console.log(book_read + " " + book_name);
+//    console.log(book_read + " " + book_name);
     
    let b2 = new book(book_name, book_author, book_pages, book_read);
     // let demo = new book("Moogly", "DK", 999, true);
@@ -64,4 +64,4 @@ function checkboxValue() {
   book_read = true;
 }
 
-// start();
+
